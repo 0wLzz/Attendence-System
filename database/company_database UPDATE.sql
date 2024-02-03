3ad24440904f763a574fe01d4aab7e517caac1e6
Owen Limantoro <145958069+0wLzz@users.noreply.github.com>
2024-02-03 22:02:56 +0800

    Update company_database UPDATE.sql

--- database/company_database UPDATE.sql
+++ database/company_database UPDATE.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 01, 2024 at 07:55 AM
+-- Generation Time: Feb 03, 2024 at 03:01 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,33 +24,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `first_name` varchar(15) DEFAULT NULL,
-  `last_name` varchar(15) DEFAULT NULL,
-  `email` varchar(225) DEFAULT NULL,
-  `bio` varchar(100) DEFAULT NULL,
-  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `first_name`, `last_name`, `email`, `bio`, `password`) VALUES
-('UD01', 'admin', 'admin', 'admin@gmail.com', 'This is admin', '0192023a7bbd73250516f069df18b500');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` varchar(11) NOT NULL,
   `picture` varchar(50) NOT NULL,
   `first_name` varchar(15) NOT NULL,
   `last_name` varchar(15) NOT NULL,
@@ -64,21 +42,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `picture`, `first_name`, `last_name`, `email`, `bio`, `password`) VALUES
-(1, 'IMG_20231202_095915 (1).jpg', '        Owenp', '        Limanto', 'what@gmail.com', '        ga tau', ''),
-(18, 'ko.JPG', ' mama', ' mama', 'michella.anjani@binus.ac.id', ' mama', ''),
-(29, 'sapilucu.jpg', 'sapi', 'sapi', 'sapi@gmail.com', 'sapi', ''),
-(33, 'IMG_20231202_095915 (1).jpg', ' cavisd', ' cavis', 'cavis@gmail.com', ' ', '9a5908e622c75c815c34cdc2cf352665');
+('U001', 'sapilucu.jpg', 'Sapi', 'Lucu', 'sapi@gmail.com', 'Hello my name is Sepi, nice to meet you all :)', 'ea51d0a9509f763419a18291c6127467'),
+('U002', 'Naturo Cat.jpeg', 'Naruto', 'Cat', 'cat@gmail.com', 'Hello my name is narita.', '619dd92c8adcd7593a4986561872db2d'),
+('U003', 'AnakKecil.jpeg', 'Anak', 'Kecil', 'bocil@gmail.com', 'Hello my name is cil. ', '4f88baa29bf63cbaaeb9466bb25329a1');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `users`
@@ -86,16 +56,6 @@ ALTER TABLE `admin`
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
